8f1b7e1436934dc3ec5b5a6f9cb1e6477ffd62d8
Muhammadrizkyfarellwahyudi <144661763+Muhammadrizkyfarellwahyudi@users.noreply.github.com>
2023-09-11 14:40:31 +0700

    Create Invoice.cpp

---
+++ Invoice.cpp
@@ -0,0 +1,18 @@
+https://vt.tiktok.com/ZSLTqkpoE/#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Program Penjumlahan \n";
+    cout<<"Masukkan Angka Pertama:";
+    cin>>a;
+    cout<<"masukkan Angka Kedua:";
+    cin>>b;
+    c=a+b;
+    cout<<"Hasil Pejumlahan adalah:"<<a+b<<endl;
+    cout<<"Hasil Pengurangan adalah:"<<a-b<<endl;
+    cout<<"Hasil Perkalian adalah:"<<a*b<<endl;
+    cout<<"Hasil Pembagian adalah:"<<a/b<<endl;
+    cout<<"Hasil Sisa bagi adalah:"<<a%b<<endl;
+    return 0;
+}
